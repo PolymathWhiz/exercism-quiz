0959e7f4783ffa83f3619243fe11f78b9828464a
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2020-05-19 21:43:55 -0500

    added normalize getter method

--- lib/exercism.dart
+++ lib/exercism.dart
@@ -1,3 +1,16 @@
-int calculate() {
-  return 6 * 7;
+class NanpParser {
+  final String number;
+
+  final regex =
+      RegExp(r'^\(?([2-9][0-9]{2})\)?[-]?([2-9](?!11)[0-9]{2})[-]?([0-9]{4})$');
+
+  final FIRST_SEQ = '+1';
+
+  NanpParser([this.number = '']);
+
+  String get normalizeNumber {
+    return number
+        .replaceAll(RegExp(r'[^\s\w]'), '')
+        .replaceAll(RegExp(r'\s+\b|\b\s'), '');
+  }
 }
